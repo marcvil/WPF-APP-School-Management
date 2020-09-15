dfff6366b629ca18bdd4e9a50105347f5471c72e
marcvil <marcvilabascara@gmail.com>
2020-09-15 12:24:59 +0200

    Initial

---
+++ WpfApp1/A4.DAL/AcademyDbContext.cs
@@ -0,0 +1,30 @@
+﻿using A4.Lib.Models;
+using Common.Lib.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace A4.DAL
+{
+    public class AcademyDbContext : DbContext
+    {
+        public AcademyDbContext(DbContextOptions<AcademyDbContext> options) : base(options)
+        {
+
+        }
+
+        private DbSet<Student> StudentDb { get; set; }
+
+        private DbSet<Subject> SubjectDb { get; set; }
+
+        private DbSet<Exam> ExamDb { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Entity>()
+                .Ignore(x => x.CurrentValidation);
+        }
+    }
+}
